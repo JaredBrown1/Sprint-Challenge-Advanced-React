125501bda455bc098c2622e65857d667e963636e
Jared Brown <jaredbrown1@hotmail.com>
2020-03-21 10:57:28 -0700

    data rendering

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,31 @@ import React from "react";
 import "./App.css";
 import axios from "axios";
 
+import PlayerCard from "./components/PlayerCard";
+
 class App extends React.Component {
+  state = {
+    players: []
+  };
+
   componentDidMount() {
-    axios.get("http://localhost:5000/api/players").then(res => {
-      console.log("from CDM", res);
-    });
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(res => {
+        console.log("from CDM", res);
+        return this.setState({ players: res.data });
+      })
+      .catch(err => console.error(err));
   }
 
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        {this.state.players.map(item => {
+          return <PlayerCard name={item.name} country={item.country} />;
+        })}
+      </div>
+    );
   }
 }
 
