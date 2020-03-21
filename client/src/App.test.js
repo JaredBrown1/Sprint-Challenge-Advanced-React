a03028e32d0df71aa5b8f3510f617633c66a25b8
Jared Brown <jaredbrown1@hotmail.com>
2020-03-21 11:33:02 -0700

    wrote test for darkmode button

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render } from "@testing-library/react";
+import DarkMode from "./components/DarkModeButton";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("has dark mode", () => {
+  const container = render(<App />);
+
+  container.getByText(/Dark Mode/);
 });
