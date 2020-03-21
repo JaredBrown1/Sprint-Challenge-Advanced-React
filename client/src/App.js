e7d616d65c630d42223f88e32c3c67dd937e64bc
Jared Brown <jaredbrown1@hotmail.com>
2020-03-21 09:39:51 -0700

    reset for flex

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,26 @@
-import React from "react";
-import "./App.css";
-import WomanList from "./components/womanList";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import DarkMode from "./components/darkModeButton";
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      women: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("http://localhost:5000/api/players")
-      .then(res => res.json())
-      .then(women => {
-        console.log("fetch: women: ", women);
-        this.setState({ women: women });
-      })
-      .catch(err => console.log("error on fetch: ", err));
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <DarkMode className="dark-mode-button" />
-        <WomanList women={this.state.women} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
